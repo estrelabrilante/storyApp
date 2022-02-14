a4ab258ef5ad6c805775719c9b3c6b851ff6d5af
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-02-14 18:06:50 -0500

    initial commit

---
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    //let story0 = "You see a Fork in the road"
+    //let choice1 = "Take a left"
+   // let choice2 = "Take a Right"
+    var selectedStory = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI();
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let usrAnswer = sender.currentTitle!;
+        let userSel = selectedStory.checkChoice(usrAnswer);
+        if  userSel{
+           updateUI()
+        }
+    }
+    
+    func updateUI(){
+        storyLabel.text = selectedStory.getLabel();
+        choice1Button.setTitle(selectedStory.getChoice1(), for: .normal);
+        choice2Button.setTitle(selectedStory.getChoice2(), for: .normal)
+        
+    }
+    
+}
+
---
+++ Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
@@ -0,0 +1,49 @@
+//
+//  StoryBrain.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct StoryBrain {
+    let stories = [
+        Story(t: "you see a fork in the road", c1:"take a left",c2:"take a right"),
+        Story(t:"you see a tiger",c1: "shout for help",c2: "play dead"),
+        
+        Story(t:"you find a tresaure chest", c1: "open it", c2: "check for traps")
+    ]
+    var uanswer = ""
+    var position = 0;
+    mutating func checkChoice(_ userAnswer: String) -> Bool  {
+    if userAnswer == (stories[position].choice1   ){
+          position += 1
+       // uanswer = stories[position].choice1
+        return true
+        }
+    else if userAnswer == stories[position].choice2
+{
+   position += 1
+        return true
+}
+    else{
+        return false
+    }
+        
+}
+    func getLabel()-> String{
+        return stories[position].title;
+        
+    }
+    func getChoice1()-> String{
+        return stories[position].choice1;
+        
+    }
+    func getChoice2()-> String{
+        return stories[position].choice2;
+        
+    }
+}
+
+
